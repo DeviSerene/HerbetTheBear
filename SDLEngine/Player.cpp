26bb4c3396e593a64f3c0ea4420561a01f39b172
Ieuan Rees <digibeard94@hotmail.com>
2017-11-06 18:28:13 +0000

    Player - Jump Function Added

--- SDLEngine/Player.cpp
+++ SDLEngine/Player.cpp
@@ -7,7 +7,15 @@ Player::Player()
 {
 	EntityPosition.x = 100;
 	EntityPosition.y = 100;
-	EntityPosition.w = 
+	EntityPosition.w = 20;
+	EntityPosition.h = 30;
+
+	playerHeath = 3;
+	playerHit = false;
+	playerFalling = false;
+	playerJumping = false;
+	jumpLimit = 40;
+
 }
 
 
@@ -18,6 +26,42 @@ Player::~Player()
 
 void Player::Input()
 {
+	
+	while (SDL_PollEvent(&key_input))
+	{
+		if (key_input.type == SDL_QUIT)
+		{
+			// Quit game
+		}
+		else if (key_input.type == SDL_KEYDOWN)
+		{
+			switch (key_input.key.keysym.sym)
+			{
+			case SDLK_SPACE:
+
+				if (playerJumping == false)
+				{
+					playerJumping = true;
+					// Initilizing this to allow the jump class to pair it with the jump limit while jumping
+					YBeforeJump = EntityPosition.y;
+				}
+				// Jump movement
+			case SDLK_a:
+
+				EntityPosition.x -= 5;
+				//left movement
+			case SDLK_d:
+
+				EntityPosition.x += 5;
+				// right movement
+			}
+		}
+	}
+
+	if (playerJumping == true)
+	{
+		PlayerJump();
+	}
 
 }
 
@@ -31,12 +75,51 @@ void Player::Draw(SpriteFactory *_sprite)
 	_sprite->Draw(fileName, EntityPosition);
 }
 
-void Player::HandleDeath()
+void Player::HandleDamage()
 {
+	if (playerHit == true)
+	{
+		playerHeath -= 1;
+		playerHit = false;
+	}
 
+	if (playerHeath == 0)
+	{
+		// Player dies
+	}
 }
 
 SDL_Rect Player::GetPlayerRect()
 {
-	return //rect;
+	return EntityPosition;
+}
+
+void Player::PlayerJump()
+{
+	if (playerFalling == false)
+	{
+		if (EntityPosition.y > YBeforeJump - jumpLimit)
+		{
+			EntityPosition.y -= 5;
+		}
+		else
+		{
+			playerFalling = true;
+			LastFrameFallingPos = EntityPosition.y;
+			EntityPosition.y -= 5;
+		}
+	}
+	else if (playerFalling == true)
+	{
+		if (LastFrameFallingPos != EntityPosition.y)
+		{
+			LastFrameFallingPos = EntityPosition.y;
+			EntityPosition.y -= 5;
+		}
+		else if (LastFrameFallingPos == EntityPosition.y)
+		{
+			playerFalling = false;
+			playerJumping = false;
+		}
+	}
 }
