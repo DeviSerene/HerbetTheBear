4d1c070a9056fd7fd4772cb913597588e005ba36
Karl Ward <s4914143@PC42862>
2017-11-07 11:38:45 +0000

    Updated animation controller.

---
+++ SDLEngine/Timer.cpp
@@ -0,0 +1,19 @@
+#include "Timer.h"
+
+Timer::Timer(float _duration) : currentTime(0.0f), duration(_duration) { }
+
+void Timer::Update(float _delta) {
+	currentTime += _delta;
+}
+
+void Timer::Wait(float _duration) {
+	duration = _duration;
+}
+
+void Timer::Reset() {
+	currentTime = 0;
+}
+
+bool Timer::Completed() {
+	return currentTime >= duration;
+}
