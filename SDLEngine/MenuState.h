27c30d98a9d8c925f100a8d87293ac81aa3fad55
Karl Ward <s4914143@PC42862>
2017-11-08 11:37:08 +0000

    asdasd

--- SDLEngine/MenuState.h
+++ SDLEngine/MenuState.h
@@ -3,12 +3,17 @@
 #include "GameState.h"
 #include "GamestateManager.h"
 
+#define BearLogo "assets/textures/Logo.png"
+
 class MenuState :
 	public GameState
 {
 public:
 	SDL_Rect playRect;
 	SDL_Rect exitRect;
+	std::vector<int> randomBg;
+	int bgW;
+	int bgH;
 
 	MenuState(GameData* _gamedata);				//constructor
 	~MenuState();								//destructor
@@ -16,6 +21,7 @@ public:
 	bool HandleSDLEvents();						//handle inputs
 	void Update(float _deltaTime);				//update 
 	void Draw();								//draw to renderer
+
 private:
 
 };
